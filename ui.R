bb7eea1422d274a303b7e95e0fe120707b8a7dd7
Jullie24 <139392709+Jullie24@users.noreply.github.com>
2023-08-14 08:52:47 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -143,6 +143,8 @@ server <- function(input, output) {
       geom_bar(stat = "identity") +
       labs(title = paste("Age Groups in the US in", selected_year),
            x = "Age Group", y = "Depression Rate") +
+      scale_fill_discrete(labels = c("X10.to.14.years.old" = "10 to 14 years old", "X15.to.19.years.old" = "15 to 19 years old", "X20.to.24.years.old" = "20 to 24 years old", "X25.to.29.years.old" = "25 to 29 years old", "X30.to.34.years.old" = "30 to 34 years old","X35.to.49.years.old" = "35 to 49 years old", "X50.to.69.years.old" = "50 to 69 years old", "X70.years.old" = "70 years old" )) +
+      scale_x_discrete(labels = c("X10.to.14.years.old" = "10 to 14 years old", "X15.to.19.years.old" = "15 to 19 years old", "X20.to.24.years.old" = "20 to 24 years old", "X25.to.29.years.old" = "25 to 29 years old", "X30.to.34.years.old" = "30 to 34 years old","X35.to.49.years.old" = "35 to 49 years old", "X50.to.69.years.old" = "50 to 69 years old", "X70.years.old" = "70 years old" )) +
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
   })
